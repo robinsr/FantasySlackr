bac3fba2b8bed099c16e08644aab821a47ed06d5
Ryan <ryan.b.robinson@gmail.com>
2013-07-04 16:02:08 -0700

    updated testserver.js

--- testserver.js
+++ testserver.js
@@ -1,16 +1,9 @@
 var http = require('http').createServer(me);
 
 
-
-function me(){
-	
-var exec = require('child_process').exec;
-exec('pwd', function callback(error, stdout, stderr){
-    console.log(stdout)
-    console.log("\n");
-    console.log(stderr)
-    console.log("\n");
-});
+function me(res,req){
+	req.end("it worked");
 }
+
 
 http.listen(8133)
