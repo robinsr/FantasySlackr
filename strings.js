a467b576c145988558f76c78efd1d67791da4684
cary <redfox137@gmail.com>
2013-07-22 20:57:02 -0700

    Worked on front end.

---
+++ strings.js
@@ -0,0 +1,49 @@
+
+
+var fantasyFlackrUtils = (function() {
+      function ajaxCall(postData, cb){
+
+
+        $.ajax({
+              url: url,
+              type: 'POST',
+        contentType: 'application/json',
+              data: JSON.stringify(postData),
+              error: function(){
+                cb(true);
+                return
+              },
+              complete: function(dat){
+                cb(null,dat.status,dat.responseText);
+                return
+              }
+            }); 
+
+      }
+
+      return{
+        publicAjaxCall: function(){
+      
+         var jsonObject = {
+          uname: $("#uname").value,
+          pass: $("#pass").value
+         };
+         ajaxCall(jsonObject,function(error,status,responseText){
+          if(error != null){
+            alert("Error");
+          }
+        else{
+          console.log(status,responseText);
+        }
+
+         });
+        }
+      }
+            
+          })();
+
+// event listener 
+$("#signin_form").submit(function(){
+  fantasyFlackrUtils.publicAjaxCall()
+
+})
