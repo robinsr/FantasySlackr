c4d1b64cd0d0de99c34c4dfc6b4ed142125034c5
Robinson, Ryan (Contractor) <ryan.b.robinson@gmail.com>
2013-07-24 15:49:01 -0700

    removed all references to redis from testserver.js so we can switch to whatever db system in the future. prolly mongodb

--- testserver.js
+++ testserver.js
@@ -5,8 +5,6 @@ var http = require('http'),
     crypto = require('crypto')
     nodeurl = require('url')
     qs = require('qs'),
-    redis = require('redis'),
-    client = redis.createClient(),
     mu = require('mu2'),
     utils = require('util'),
     serveStatic = require('./serveStatic'),
@@ -14,50 +12,17 @@ var http = require('http'),
     templates = require('./templates'),
     //oauth = require('./oauth'),
     oauth = require('./oauthtest'),
-	appMonitor = require('./appMonitor');
+	appMonitor = require('./appMonitor'),
+    db = require('./dbModule');
 
 mu.root = __dirname + '/'
  
 
-function validateSession(n, s, cb) {
-    console.log(n, s)
-
-    // so that demos work regardless of sessions. its a problem if there are two people
-    // trying to use the demo account at once
-    if (n === 'demo') {
-        console.log('demo session ' + s);
-        cb(true);
-
-        // regular session validation
-    } else {
-        client.exists("session:" + n, function (ex, r) {
-            if (r === 0) {
-                cb(false);
-                return;
-            } else {
-                client.get("session:" + n, function (err, r) {
-                    if (r == s) {
-                        cb(true);
-                        return;
-                    } else {
-                        cb(false);
-                        return;
-                    }
-                })
-            }
-        });
-    }
-}
-
-
-
-
-
 function constructDashboard(req,res){
 	var query = qs.parse(nodeurl.parse(req.url).query);
-	validateSession(query.user,query.sess,function(sessionValid){
+	db.validateSession(query.user,query.sess,function(sessionValid){
 		if (sessionValid){
-            client.get('fantasyuser:'+query.user,function(err,result){
+            db.getFromUserDb(query.user,function(err,result){
                 if (err || (result == null)){
                     templates.sendErrorResponse(res,"We're having some problems.","Please try again later.","err010");
 					appMonitor.sendMessage("error","err010, db read error");
@@ -96,14 +61,7 @@ function constructDashboard(req,res){
 
 
 
-function setTemporaryToken(token,secret,username){
-	var set = {
-		oauth_token_secret: secret,
-		username: username
-	}
-	client.set("fantasy:oauth:"+token,JSON.stringify(set),function(err,r){});
-	client.expire("fantasy:oauth:"+token, 1800);
-}
+
 
 
 	// exchanges req token and tok verifier for access token 
@@ -111,7 +69,7 @@ function handleApiCallback(req,res){
     var yahooCb = qs.parse(nodeurl.parse(req.url).query);     
            
         // find token_secret in db
-    client.get("fantasy:oauth:"+yahooCb.oauth_token,function(err,result){
+    db.getTemporaryToken(yahooCb.oauth_token,function(err,result){
         if (err){
             appMonitor.sendMessage("error","failed to find token in database. step 2 of oauth")
             templates.sendErrorResponse(res,"There was an error setting up your account","Please try again later","err 001 - db error");
@@ -119,7 +77,9 @@ function handleApiCallback(req,res){
             return
         } else {
             var storedData = JSON.parse(result);
-            //oauth.getAccess(yahooCb,storedData,function(err,oauthResponse){
+
+
+    // GETS OAUTH ACCESS TOKEN
 
             // begin brevity...
             var getT = yahooCb.oauth_token,
@@ -136,20 +96,25 @@ function handleApiCallback(req,res){
                     console.log('***** there was an error *****');
                     console.log(err);
                 } else {
-                    client.get("fantasyuser:"+storedData.username,function(db_err,dat){
+
+    // STORES OAUTH ACCESS TOKEN
+                    var newdata = {
+                        access_token = token;
+                        access_token_secret = secret;
+                        session_handle = result.oauth_session_handle;
+                        guid = result.xoauth_yahoo_guid;
+                    }
+
+                    db.updateUserDb(storedData.username,newdata,function(db_err,dat){
                         if (db_err){
                             templates.sendErrorResponse(res,"There was an error setting up your account","Please try again later", "err 003 - db error");
 							appMonitor.sendMessage("error","err003, redis could not get users info");
                         } else {
-                            var userdata = JSON.parse(dat);
-                            console.log('oauthResponse');
-                            console.log(token);
-                            userdata.access_token = token;
-                            userdata.access_token_secret = secret;
-                            userdata.session_handle = result.oauth_session_handle;
-                            userdata.guid = result.xoauth_yahoo_guid;
 
-                            slackr_utils.requestHash(function(session_val){
+    // CREATES SESSION 
+                            db.createSession(function(session_val){
+    // REDIRECTS
+
                                 res.writeHead(302, { 'Location': 'dashboard?user='+storedData.username+'&sess='+session_val });
                                 res.end(); 
                             });                    
@@ -161,58 +126,35 @@ function handleApiCallback(req,res){
     });    
 }
 
-function storeOauthData(userdata,oauthResponse){
-    userdata.oauth_access_token = oauthResponse.oauth_token;
-    userdata.oauth_access_token_secret = oauthResponse.oauth_token_secret;
-    userdata.oauth_session_handle = oauthResponse.oauth_session_handle;
-    userdata.xoauth_yahoo_guid = oauthResponse.xoauth_yahoo_guid;
-    client.set("fantasyuser:"+storedData.username,JSON.stringify(userdata),function(err){
-        if (err){
-            templates.sendErrorResponse(res,"There was an error setting up your account","Please try again later", "err 005");
-			appMonitor.sendMessage("error","err005, redis could not store users oauth");
-        } else {
-            var session_key = "session:"+storedData.username;
-            slackr_utils.requestHash(function(session_val){
-                client.set(session_key, session_val, function () {
-                client.expire(session_key, 1800);
-                client.del("fantasy:oauth:"+dataFromYahooCallback.oauth_token);
-                return;
-                });
-            });
-        }
-    });
-}
-
 	// sets session keys for users and (in future) get new oauth token
 function login(req, res, data) {
     var p = /[0-9a-f]{32}/
     var uname = "fantasyuser:" + data.uname;
-    client.get(uname, function (err, r) {
+
+    // GETS USER OBJECT FROM DB
+
+    db.getFromUserDb(data.uname,function(err, r) {
         if (err) {
             console.log('error');
 			templates.sendErrorResponse(res,"There was an error logging into your account","Please try again later", "err 008");
 			appMonitor.sendMessage("error","login, redis could not get user data");
         } else {
+
+    // CHECKS IF PASSWORD MATCHES HASH
             if (r !== null) {
                 var user_object = JSON.parse(r);
                 var return_object = {};
                 var concat_pass = data.pass + user_object.salt;
                 var hashed_pass = crypto.createHash('md5').update(concat_pass).digest('hex');
                 if (hashed_pass == user_object.pass) {
-                    slackr_utils.requestHash(function (session_val) {
+
+    // SETS SESSION - needs token to set session
+
+                    db.createSession(uname,token,function(session_val){
                         return_object.sessionid = session_val;
-                        if (user_object.email) {
-                            return_object.email = user_object.email
-                        } else {
-                            return_object.email = 'no email';
-                        }
-                        var session_key = "session:" + data.uname;
-                        client.set(session_key, session_val, function () {
-                            client.expire(session_key, 1800);
-                            res.writeHead(302, { 'Location': 'dashboard?user='+data.uname+'&sess='+session_val });
-                            res.end(JSON.stringify(return_object));
-                            return;
-                        });
+                        res.writeHead(200, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify(return_object));
+                        return;
                     });
                 } else {
                     res.writeHead(400, { 'Content-Type': 'text/plain' });
@@ -229,39 +171,22 @@ function login(req, res, data) {
 }
 function logout(req, res) {
 	var query = qs.parse(nodeurl.parse(req.url).query);
-    var uname = "session:" + query.n;
-    client.get(uname, function (err, c) {
-        if (err) {
-			appMonitor.sendMessage('error','logout1, redis error');
-		}
-        if (c) {
-            client.del(uname, function () {
-        	var d = {
-			header: "Goodbye",
-			message1: "You have been logged out",
-			message2: ""
-		}
-		var html = ''
-		mu.compileAndRender('genericMessagePage.html', d).on('data', function (data) {
-		    	html += data.toString();
-	  	}).on('end', function(){
-	  		res.writeHead(200);
-		    	res.end(html);
-	  	});
-            });
+    db.destroySession(query.n,function(er){
+        if (er){
+            template.sendErrorResponse(res,"Uh-Oh","Something went wrong","","err 013");
+            return
         } else {
-            templates.sendErrorResponse(res,'You were logged in? Maybe your session expired','')
-			return;
+            templates.logout(res);
+            return
         }
-    });
+    });        
 }
 
 	// creates user in database and begins oauth process, sends link to yahoo auth page if successful
 function createUser(req, res, uname, upass, uemail) {
-  var redisUname = "fantasyuser:" + uname;
 
     // check if user already exists
-  client.exists(redisUname, function (ex, r) {
+  db.checkIfUserExists(uname, function (ex, r) {
     if (r == 1) {
       templates.sendErrorResponse(res,"Account already exists for "+uname,"Please try a different username")
       return;
@@ -280,7 +205,7 @@ function createUser(req, res, uname, upass, uemail) {
           } else {
 
             // store request token, token secret, and username for later lookup
-            setTemporaryToken(oauth_token,oauth_token_secret,uname);
+            db.setTemporaryToken(oauth_token,oauth_token_secret,uname);
 
             // create user object to be stored
             var user_setup = {
@@ -293,10 +218,8 @@ function createUser(req, res, uname, upass, uemail) {
               xoauth_request_auth_url: oauth_url
             };
 
-            var udata = JSON.stringify(user_setup);
-
             // store user object
-            client.set("fantasyuser:"+uname, udata, function (err, rr) {
+            db.addToUserDb(uname, JSON.stringify(user_setup), function (err, rr) {
 
               // send success or error page
               if (err) {
@@ -308,13 +231,7 @@ function createUser(req, res, uname, upass, uemail) {
                   uname: uname,
                   url: oauth_url
                 }
-                var html = ''
-                mu.compileAndRender('successpage.html', d).on('data', function (data) {
-                  html += data.toString();
-                }).on('end', function(){
-                  res.writeHead(200);
-                  res.end(html);
-                });
+                templates.success(res,d);
                 return;
               }
             });
@@ -326,22 +243,6 @@ function createUser(req, res, uname, upass, uemail) {
 }
 
 
-
-
-	// These function serve as a launching points into more server functions, 
-	// a sort of intermediary between server code and the incoming http request
-	// They are genrally called from "handler"
-function loginFormHandler(req,res){
-	console.log('login form accepted');
-	var fullbody = '';
-
-	req.on('data', function(chunk){
-		fullbody += chunk
-	});
-	req.on('end',function(){
-		login(req,res,querystring.parse(fullbody));
-	})
-} 
 function signupFormHandler(req,res){
 	console.log('signup form accepted');
 	var fullbody = '';
@@ -366,7 +267,7 @@ function ajaxLogin(req,res){
     req.on('end',function(){
         var userdata = JSON.parse(bodyText);
 
-        client.get("fantasyuser:"+userdata.name,function(err,c){
+        db.getFromUserDb(userdata.name,function(err,c){
             if (err) {
                 res.writeHead(500);
                 res.end("no user account found")
@@ -380,6 +281,8 @@ function ajaxLogin(req,res){
                 res.end(JSON.stringify(dat));
             }
         })
+
+        
         userdata.name
         userdata.password
     })
@@ -394,9 +297,6 @@ function handler(req,res){
 	if (p == '/apicallback'){
         	handleApiCallback(req,res);
         	return;
-	} else if (p == '/loginForm'){
-		loginFormHandler(req,res);
-		return;
 	} else if (p == '/signupForm'){
 		signupFormHandler(req,res);
 		return;
