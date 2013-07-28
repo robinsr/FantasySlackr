489c1547c9ed43dd6bd758f41f743c4473337163
robinsr <ryan.b.robinson@gmail.com>
2013-07-28 07:47:32 +0000

    added condition in cases where message is null

--- appMonitor.js
+++ appMonitor.js
@@ -1,32 +1,30 @@
 var http = require('http');
 
 var sendMessage = function(level,message){
-    message = message.replace(/\"/g,'');
-    console.log('starting error report: '+message)
-    var postData = {
-    	level: level,
-    	message: message
-    }
-    var postOptions = {
-        host: 'localhost',
-        port: 8136,
-	path: '/fantasyslackr',
-        method: 'POST',
-        headers: {
-            'Content-Type' :'application/json'
+    if (level && message){
+        message = message.replace(/\"/g,'');
+        var postData = {
+        	level: level,
+        	message: message
         }
-    };
-    var keyReq = http.request(postOptions,function(errorRes){
-        console.log('starting request');
-        }).on('end',function(){
-            console.log("sent to App Monitor: ",level,message);
-        }).on('data',function(c){
-            console.log('got error report errorRes, data')
-        }).on('error',function(er){
-            console.log('appMonitor Error - '+er);
-        });
-    keyReq.write(JSON.stringify(postData));
-    keyReq.end();
+        var postOptions = {
+            host: 'localhost',
+            port: 8136,
+    	path: '/fantasyslackr',
+            method: 'POST',
+            headers: {
+                'Content-Type' :'application/json'
+            }
+        };
+        var keyReq = http.request(postOptions,function(errorRes){
+            }).on('end',function(){
+            }).on('data',function(c){
+            }).on('error',function(er){
+                console.log('appMonitor Error - '+er);
+            });
+        keyReq.write(JSON.stringify(postData));
+        keyReq.end();
+    }
 }
 
 module.exports.sendMessage = sendMessage;
