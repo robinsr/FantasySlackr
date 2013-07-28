c43ba8b3096dbb2bee3a77f57ba27030716c50e2
robinsr <ryan.b.robinson@gmail.com>
2013-07-28 00:53:01 +0000

    Stable createNewUser and login, process seems to be working fine

--- testserver.js
+++ testserver.js
@@ -5,77 +5,58 @@ var http = require('http'),
     crypto = require('crypto')
     nodeurl = require('url')
     qs = require('qs'),
-    mu = require('mu2'),
     utils = require('util'),
     serveStatic = require('./serveStatic'),
     slackr_utils = require('./slackr_utils'),
-    templates = require('./templates'),
     //oauth = require('./oauth'),
     oauth = require('./oauthtest'),
 	appMonitor = require('./appMonitor'),
     db = require('./dbModule');
 
-mu.root = __dirname + '/'
- 
-
 	// exchanges req token and tok verifier for access token 
 function handleApiCallback(req,res){
+    appMonitor.sendMessage('debud','recevied yahoo callback');
+    res.writeHead(200);
+    res.end('Got it, thanks');
     var yahooCb = qs.parse(nodeurl.parse(req.url).query);     
            
         // find token_secret in db
     db.getTemporaryToken(yahooCb.oauth_token,function(err,result){
         if (err){
-            appMonitor.sendMessage("error","failed to find token in database. step 2 of oauth")
-            templates.sendErrorResponse(res,"There was an error setting up your account","Please try again later","err 001 - db error");
 			appMonitor.sendMessage("error","err001, failed to find token in database");
             return
         } else {
             var storedData = JSON.parse(result);
-
-
-    // GETS OAUTH ACCESS TOKEN
-
-            // begin brevity...
-            var getT = yahooCb.oauth_token,
-                getV = yahooCb.oauth_verifier,
-                getS = storedData.oauth_token_secret;
-            // end brevity
-
-            oauth.getAccess(getT,getV,getS,function(err,token,secret,result){
+            // GETS OAUTH ACCESS TOKEN
+            oauth.getAccess(yahooCb.oauth_token,yahooCb.oauth_verifier,storedData.oauth_token_secret,function(err,token,secret,result){
                 if (err){
-                    templates.sendErrorResponse(res,"There was an error setting up your account","Please try again later", "err 002 - oauth connection problem");
 					appMonitor.sendMessage("error","err002, oauth connection problem");
-                    console.log('***** there was an error *****');
-                    console.log(err);
                 } else {
-
-    // STORES OAUTH ACCESS TOKEN
-                    var newdata = {
-                        access_token: token,
-                        access_token_secret: secret,
-                        session_handle: result.oauth_session_handle,
-                        guid: result.xoauth_yahoo_guid
-                    }
-
-                    db.updateUserDb(storedData.username,newdata,function(db_err){
-                        if (db_err){
-                            templates.sendErrorResponse(res,"There was an error setting up your account","Please try again later", "err 003 - db error");
-							appMonitor.sendMessage("error","err003, redis could not get users info");
-                        } else {
-
-    // CREATES SESSION 
-                            db.createSession(storedData.username,token,function(err,session_val){
-    // REDIRECTS
-
-                                res.writeHead(302, { 'Location': 'dashboard?user='+storedData.username+'&sess='+session_val });
-                                res.end(); 
-                            });                    
-                        }
-                    });
+                    // STORES OAUTH ACCESS TOKEN
+                    storeAccessResult(storedData.username,token,secret,result);
+                    // AT THIS POINT, PINGING login WOULD RETURN A SESSION, NOT "Unauthorized User"
                 }
             });                         
         }
     });    
+}
+
+function storeAccessResult(username,token,secret,result){
+    var newdata = {
+        access_token: token,
+        access_token_secret: secret,
+        access_token_expires : parseInt(new Date().getTime()) + (parseInt(result.oauth_expires_in) * 1000),
+        session_handle: result.oauth_session_handle,
+        session_handle_expires: parseInt(new Date().getTime()) + (parseInt(result.oauth_authorization_expires_in) * 1000),
+        guid: result.xoauth_yahoo_guid
+    }
+    db.updateUserDb(username,newdata,function(db_err){
+        if (db_err){
+            appMonitor.sendMessage("error","err003, update user failed");
+        } else {
+            appMonitor.sendMessage('success','store access result success')
+        }
+    });
 }
 
 function logout(req,res,data) {
@@ -94,120 +75,147 @@ function logout(req,res,data) {
 	// creates user in database and begins oauth process, sends link to yahoo auth page if successful
 function createUser(req, res, userdata) {
 
-	var expectedData = ["uname","upass","uemail"];
+    checkdata(req,res,["uname","upass","uemail"],userdata,function(){
+        var hash_salt = slackr_utils.requestHashAsync(32);
+    	var hashed_pass_and_salt = crypto.createHash('md5').update(userdata.upass + hash_salt).digest('hex');
+        console.log(hash_salt);
+        console.log(hashed_pass_and_salt);
 
-	expectedData.forEach(function(piece){
-		if (typeof userdata[piece] == 'undefined'){
-			res.writeHead(400, {'Content-Type':'application/json'});
-			res.end("request parameter missing: "+piece);
-			return
-		}
-	});
+        // check if user already exists
+      	db.getFromUserDb(userdata.uname, function (err, r){
+    	    if (err) {
+    	        res.writeHead(500, { 'Content-Type': 'application/json' });
+    	        res.end(JSON.stringify({error:'Database Error'}));
+    	        return
+    	    } else if (r) {
+    	    	res.writeHead(400, { 'Content-Type': 'application/json' });
+    	        res.end(JSON.stringify({error:'Account already exists for '+userdata.uname}));
+    	        return
+        	} else {
+    	        oauth.getToken(function(oauth_err,oauth_token,oauth_token_secret,oauth_url){
+    	          	if (oauth_err != null){
+    		            res.writeHead(500, { 'Content-Type': 'application/json' });
+    			        res.end(JSON.stringify({error:'Oauth Error'}));
+    			        return
+    	          	} else {
+    	            	// store request token, token secret, and username for later lookup
+    		            db.setTemporaryToken(oauth_token,oauth_token_secret,userdata.uname);
 
-	var hashed_pass_and_salt = crypto.createHash('md5').update(upass + slackr_utils.requestHashAsync(32)).digest('hex');
+    		            // create user object to be stored
+    		            var user_setup = {
+    		              name: userdata.uname,
+    		              email: userdata.uemail,
+    		              pass: hashed_pass_and_salt,
+    		              salt: hash_salt,
+    		            };
 
-    // check if user already exists
-  	db.getFromUserDb(uname, function (ex, r){
-	    if (err) {
-	        res.writeHead(500, { 'Content-Type': 'application/json' });
-	        res.end(JSON.stringify({error:'Database Error'}));
-	        return
-	    } else if (r == 1) {
-	    	res.writeHead(400, { 'Content-Type': 'application/json' });
-	        res.end(JSON.stringify({error:'Account already exists for '+uname}));
-	        return
-    	} else {
-	        oauth.getToken(function(oauth_err,oauth_token,oauth_token_secret,oauth_url){
-	          	if (oauth_err != null){
-		            res.writeHead(500, { 'Content-Type': 'application/json' });
-			        res.end(JSON.stringify({error:'Oauth Error'}));
-			        return
-	          	} else {
-	            	// store request token, token secret, and username for later lookup
-		            db.setTemporaryToken(oauth_token,oauth_token_secret,uname);
-
-		            // create user object to be stored
-		            var user_setup = {
-		              name: uname,
-		              email: uemail,
-		              pass: hashed_pass_and_salt,
-		              salt: hash_salt,
-		            };
-
-            		// store user object
-            		db.addToUserDb(user_setup, function (err) {
-						if (err) {
-			                res.writeHead(500, { 'Content-Type': 'application/json' });
-					        res.end(JSON.stringify({error:'Database Error'}));
-					        return
-		              	} else {
-		              		res.writeHead(200, { 'Content-Type': 'application/json' });
-                			res.end(JSON.stringify({ uname: uname, url: oauth_url }));
-              			}
-            		});
-          		}
-    		});
-      	}
-	});
+                		// store user object
+                		db.addToUserDb(user_setup, function (err) {
+    						if (err) {
+    			                res.writeHead(500, { 'Content-Type': 'application/json' });
+    					        res.end(JSON.stringify({error:'Database Error'}));
+    					        return
+    		              	} else {
+    		              		res.writeHead(200, { 'Content-Type': 'application/json' });
+                    			res.end(JSON.stringify({ uname: userdata.uname, url: oauth_url }));
+                  			}
+                		});
+              		}
+        		});
+          	}
+    	});
+    });
 }
 
-
-function login(req,res,userdata){
-
-	var expectedData = ["uname","upass"];
-
-	expectedData.forEach(function(piece){
-		if (typeof userdata[piece] == 'undefined'){
-			res.writeHead(400, {'Content-Type':'application/json'});
-			res.end("request parameter missing: "+piece);
-			return
-		}
-	});
-
-    db.getFromUserDb(userdata.uname,function(err,user_object){
-
-    	var token = user_object.access_token,
-            secret = user_object.access_token_secret,
-            handle = user_object.session_handle;
-
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({error:'Database Error'}));
-            return
-        } else if (user_object == null) {
-            res.writeHead(400, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({error:'No User Account Found'}));
-            return
-        } else if ((userdata.pass+user_object.salt) != crypto.createHash('md5').update(concat_pass).digest('hex')) {
-        	res.writeHead(400, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({error:'Invalid Password'}));
-            return
-        } else if (token == 'undefined' || secret  'undefined' || handle == 'undefined'){
-            res.writeHead(400, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({error:'Unauthenticated User'}));
+function checkdata(req,res,expectedData,actualdata,cb){
+    var checked = [];
+    expectedData.forEach(function(piece){
+        if (typeof actualdata[piece] == 'undefined'){
+            res.writeHead(400, {'Content-Type':'application/json'});
+            res.end("request parameter missing: "+piece);
             return
         } else {
-        	oauth.refreshToken(token,secret,handle,function(err,token,secret,result){
-                if (err){
-                    res.writeHead(500, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({error: 'could not refresh token'}));
-                    return
-                } else {
-                    db.createSession(user_object.name,token,function(err,hash){
-                        if (err) {
-                            res.writeHead(500, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify({error: 'could not create session'}));
-                            return
-                        } else {
-                            res.writeHead(200, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify({session:hash}));
-                            return
-                        }
-                    });
-                }
-            });
+            checked.push(piece);
+            if (checked.length == expectedData.length){
+                cb();
+            }
         }
     });
+}
+function checkUsersTokenExp(user_object,cb){
+    var token = user_object.access_token,
+    token_ex = user_object.access_token_expires,
+    secret = user_object.access_token_secret,
+    handle = user_object.session_handle,
+    handle_ex = user_object.session_handle_expires,
+    now = new Date().getTime();
+    console.log(now);
+    console.log(token_ex);
+
+    if ((typeof token_ex == 'undefined') || (now > token_ex)){
+        oauth.refreshToken(token,secret,handle,function(err,newtoken,newsecret,result){
+            if (err) {
+                cb(1);
+            } else {
+                cb(null,newtoken,newsecret,result);
+            }
+        });
+    } else {
+        cb(null,token,secret);
+    }
+}
+
+function login(req,res,userdata){
+    checkdata(req,res,["uname","upass"],userdata,function(){
+
+        db.getFromUserDb(userdata.uname,function(err,user_object){
+
+        	
+
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({error:'Database Error'}));
+                return
+            } else if (user_object == null) {
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({error:'No User Account Found'}));
+                return
+            } else if (user_object.pass != crypto.createHash('md5').update(userdata.upass + user_object.salt).digest('hex')) {
+            	res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({error:'Invalid Password'}));
+                return
+            } else if (typeof user_object.access_token == 'undefined'){
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({error:'Unauthenticated User'}));
+                return
+            } else {
+
+                checkUsersTokenExp(user_object,function(err,token,secret,result){
+                    if (err){
+                        res.writeHead(500, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify({error: 'Could Not Refresh Token'}));
+                        return
+                    } else {
+                        db.createSession(user_object.name,token,function(err,hash){
+                            if (err) {
+                                res.writeHead(500, { 'Content-Type': 'application/json' });
+                                res.end(JSON.stringify({error: 'could not create session'}));
+                                return
+                            } else {
+                                res.writeHead(200, { 'Content-Type': 'application/json' });
+                                res.end(JSON.stringify({session:hash}));
+                                return
+                            }
+                        });
+                        if (result){
+                            storeAccessResult(user_object.name,token,secret,result);
+                            console.log('login, got result, stored newdata');
+                        }
+                    }
+                });
+            }
+        });
+});
 }
 function respondOk(req,res,data){
 	var ret = {
@@ -223,6 +231,8 @@ function handler(req,res){
 	slackr_utils.ajaxBodyParser(req,function(data){
 		req.url = req.url.replace('/FantasyAutomate', '');
 	    var p = nodeurl.parse(req.url).pathname;
+        var p1 = p.split('/')[1];
+        console.log(p1);
 
 	    if (p == '/apicallback'){
         	handleApiCallback(req,res);
@@ -230,6 +240,9 @@ function handler(req,res){
 	    } else if (p == '/method/login'){
 	        login(req,res,data);
 	        return
+        } else if (p == '/method/logout'){
+            logout(req,res,data);
+            return
 	    } else if (p == '/method/createNewUser'){
 	        createUser(req,res,data);
 	        return
@@ -258,8 +271,8 @@ function handler(req,res){
 	        respondOk(req,resd,ata);
 	        return
 	    } else {
-	        res.writeHead(400);
-	        res.end({error:"invalid method"});
+	        serveStatic.serveStatic(req,res);
+            return
 	    }
 	});
 }
