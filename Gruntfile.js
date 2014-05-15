22e17760c852d60f365b5fdccff6d3b419f82ea3
Ryan <ryan.b.robinson@gmail.com>
2014-05-14 23:37:31 -0700

    Adding Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    jshint: {
+      files: ['src/**/*.js','!src/public/**/*.js', '!src/models/backlog/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        },
+        expr: true,
+        evil: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.registerTask('test', ['jshint', 'mocha']);
+
+  grunt.registerTask('default', ['jshint']);
+
+};
