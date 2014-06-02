ca1b43133bd2b6dd490416e6b56bfaefe950c3e5
Ryan <ryan.b.robinson@gmail.com>
2014-06-01 22:41:26 -0700

    Added uglify, cssmin, sass to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,50 @@ module.exports = function(grunt) {
         expr: true,
         evil: true
       }
+    },
+    concat: {
+      dist: {
+        src: [
+        'node_modules/async/lib/async.js',
+        'src/public/src/js/jquery-1.8.3.min.js',
+        'src/public/src/js/knockout_full.js',
+        'src/public/src/js/utils.js',
+        'src/public/src/js/binding.js',
+        'src/public/src/js/viewmodel.js'
+        ],
+        dest: 'src/public/dist/js/bundle.js'
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'src/public/dist/js/bundle.min.js': ['src/public/dist/js/bundle.js']
+        }
+      }
+    },
+    sass: {
+
+    },
+    cssmin: {
+
+    },
+    watch: {
+      files: [
+        'src/public/src/js/*.js'
+      ],
+      tasks: ['concat']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha');
   grunt.registerTask('test', ['jshint', 'mocha']);
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint','concat','uglify']);
 
 };
